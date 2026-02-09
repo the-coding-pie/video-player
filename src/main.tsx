3efa30106973aa0327b5ec5f547921cbb05fef8a
aravindttand <aravindk@thetravelagentnextdoor.com>
2026-02-09 12:08:50 +0530

    fix: build fix

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router";
 import router from "./routes";
-import VideoPlayer from "./components/VideoPlayer";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
